5430be3b8d012bf8b7c4329952e9eec28e923eac
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-19 01:16:44 +0530

    generated file: `next.config.js`

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+}
+
+module.exports = nextConfig
